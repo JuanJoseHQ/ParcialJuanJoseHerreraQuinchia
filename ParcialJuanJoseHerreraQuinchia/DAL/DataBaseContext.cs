efedf2bca562a8ee2ebfdeaea095ad5d4992fb49
Juan José <juanjose2815@gmail.com>
2023-05-21 15:38:27 -0500

    Create Entity Tickets

---
+++ ParcialJuanJoseHerreraQuinchia/DAL/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcialJuanJoseHerreraQuinchia.DAL.Entities;
+
+namespace ParcialJuanJoseHerreraQuinchia.DAL
+
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
---
+++ ParcialJuanJoseHerreraQuinchia/DAL/Entities/Tickets.cs
@@ -0,0 +1,20 @@
+﻿namespace ParcialJuanJoseHerreraQuinchia.DAL.Entities
+{
+    public class Tickets
+    {
+        //Attributes
+        [Key]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha_Uso")]
+        public DateTime? UseDate { get; set; }
+
+        [Display(Name = "Usada")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public Boolean IsUsed { get; set; }
+
+        [Display(Name = "Localidad")]
+        public String? EntranceGate { get; set; }
+
+    }
+}
