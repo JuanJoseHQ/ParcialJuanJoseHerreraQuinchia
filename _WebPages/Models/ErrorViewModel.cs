77365b5b1275ece54b05cb3b5b855be4d1905313
Juan José <juanjose2815@gmail.com>
2023-05-22 13:34:47 -0500

    Delete Projects Duplicate

--- _WebPages/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _WebPages.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
